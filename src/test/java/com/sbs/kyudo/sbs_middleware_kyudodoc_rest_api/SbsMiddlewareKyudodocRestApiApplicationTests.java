942063fe1e0ed57de3fc008a4d4fc7ca661c8250
SONE NSIME Ngole <sone-nsime.ngole@soprabanking.com>
2025-08-26 08:28:37 +0200

    initial commit

---
+++ src/test/java/com/sbs/kyudo/sbs_middleware_kyudodoc_rest_api/SbsMiddlewareKyudodocRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbs.kyudo.sbs_middleware_kyudodoc_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbsMiddlewareKyudodocRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
