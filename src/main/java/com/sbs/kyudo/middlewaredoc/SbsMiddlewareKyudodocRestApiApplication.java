9844a9b8e1e58e2e01f4a9d0f4eafa03663e7c0d
SONE NSIME Ngole <sone-nsime.ngole@soprabanking.com>
2025-08-26 11:32:08 +0200

    change package name

---
+++ src/main/java/com/sbs/kyudo/middlewaredoc/SbsMiddlewareKyudodocRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.sbs.kyudo.middlewaredoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbsMiddlewareKyudodocRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbsMiddlewareKyudodocRestApiApplication.class, args);
+	}
+
+}
